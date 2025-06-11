e7b25efaf0ac7b48dd2f46766344ad1b5bd102b2
jmparga <jorgemparga@gmail.com>
2025-06-10 18:57:37 -0600

    V1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-car_data = pd.read_csv('C:/Users/dammy/vehicles_env/Sprint7/vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Construir histograma')
 if hist_button:
     st.write('Creacion de un histograma para el conjunto')
